ee5091693ab618a005a7e0941a18d9dfbd82d7e8
Tania Marin <nicte.tania@gmail.com>
2019-10-07 18:36:40 -0500

    Added qr reader (test)

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -12,6 +12,7 @@ import Landing from './components/Landing/Landing';
 import Level from './components/Level';
 import Rewards from './components/Rewards/Rewards';
 import CreateNew from './components/Centers/CreateNew';
+import Test from './components/Profile/Test';
 
 const Router = () => (
   <BrowserRouter>
@@ -24,6 +25,7 @@ const Router = () => (
       <Route exact path="/rewards" component={Rewards}/>
       <Route exact path="/create-new" component={CreateNew}/>
       <Route exact path="/profile" component={Profile}/>
+      <Route exact path="/test" component={Test}/>
       <Route exact path="/centers" component={Centers}/>
       <Route exact path="/center/:id" component={Center}/>
       <Route exact path="/centers/:categoryCenter" component={CentersCategory}/>
